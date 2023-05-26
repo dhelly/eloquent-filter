96b257706e97716c6d596352fee87653e97d6f18
Jaqueline Fernades <dhelly@gmail.com>
2023-05-25 22:36:24 -0300

    configuração inicial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,27 @@
     </x-slot>
 
     <div class="py-12">
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-2">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="{{ route('dashboard') }}" method="GET">
+                        @csrf
+                        <!-- Name -->
+                        <div>
+                            <input id="search" class="block mt-1 w-full" type="text" value="{{ old('search') }}"/>
+                        </div>
+                    </div>
+                    </form>
+            </div>
+        </div>
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    @foreach ($users as $user)
+                        <li>{{ $user->id }} :: {{ $user->name }} :: {{ $user->email }}</li>
+                    @endforeach
                 </div>
             </div>
         </div>
